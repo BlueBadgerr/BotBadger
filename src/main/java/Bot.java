455efecc6c0cc5ffc2772d0a233c4e6c16877046
BlueBadgerr <gabriel_lee_92@hotmail.com>
2021-09-27 22:31:44 +0930

    cleanup

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,8 +2,6 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -11,15 +9,15 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
-import java.net.Socket;
 
 public class Bot extends ListenerAdapter {
+    private static JDA jda;
     private static Summoner summoner;
 
     public static void main(String[] args) throws LoginException {
         String token = System.getenv("TOKEN");
 
-        JDA jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+        jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new Bot())
                 .setActivity(Activity.watching("you"))
                 .build();
