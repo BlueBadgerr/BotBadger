54821fe08d6aa61721fd7a4ec062dc6fa97bd77a
BlueBadgerr <gabriel_lee_92@hotmail.com>
2021-10-02 23:36:46 +0930

    refactored database control

--- src/main/java/org/bluebadger/Bot.java
+++ src/main/java/org/bluebadger/Bot.java
@@ -10,35 +10,28 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.bluebadger.libraries.Database;
 import org.bluebadger.summoner.Summoner;
 
 import javax.security.auth.login.LoginException;
 import java.sql.*;
 
 public class Bot extends ListenerAdapter {
-    private static final String INSERT_QUERY = "INSERT INTO discord(id, yoru_slap_count) VALUES(?, ?)";
-    private static final String SELECT_ALL_QUERY = "SELECT * FROM discord";
-    private static final String SELECT_USER_QUERY = "SELECT * FROM discord WHERE id = '?'";
-
-    private static JDA jda;
-    private static Connection connection;
     private static Summoner summoner;
 
     public static void main(String[] args) throws LoginException, SQLException {
         String token = System.getenv("TOKEN");
 
-        jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+        JDA jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new Bot())
                 .setActivity(Activity.watching("you"))
                 .build();
 
+        // Connect to Postgres database
+        Database.connect(System.getenv("JDBC_DATABASE_URL"));
+
         // Bot action classes
         summoner = new Summoner();
-
-        // Database
-        connection = DriverManager.getConnection(System.getenv("JDBC_DATABASE_URL"));
-//        Statement statement = conn.createStatement();
-//        ResultSet resultSet = statement.executeQuery(SELECT_ALL_QUERY);
 
         // This can take up to 1 hour to show up in the client
         jda.updateCommands()
@@ -70,35 +63,8 @@ public class Bot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        Message msg = event.getMessage();
-
-        if (msg.getContentRaw().equals("add")) {
-            try {
-                Integer slapCount = getYoruSlapCount("122354953162457088");
-                if (slapCount == null) {
-                    PreparedStatement insertStatement = connection.prepareStatement(INSERT_QUERY);
-
-                    insertStatement.setString(1, msg.getMember().getUser().getId());
-                    insertStatement.setInt(2, 1);
-                    insertStatement.executeUpdate();
-                } else {
-                    Statement statement = connection.createStatement();
-                    slapCount++;
-                    statement.executeUpdate(
-                            String.format("UPDATE discord SET yoru_slap_count = '%d' WHERE id = '122354953162457088'", slapCount));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (msg.getContentRaw().equals("get")) {
-            try {
-                System.out.println("Total slap = " + getYoruSlapCount("122354953162457088"));
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        // Do nothing for now
+//        Message msg = event.getMessage();
 
         // Used to reply to messages
 //        MessageChannel channel = event.getChannel();
@@ -109,16 +75,5 @@ public class Bot extends ListenerAdapter {
 //        eb.setDescription("HAHAHAHA");
 //
 //        channel.sendMessageEmbeds(eb.build()).queue();
-    }
-
-    private Integer getYoruSlapCount(String user) throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(String.format("SELECT * FROM discord WHERE id = '%s'", user));
-
-        if (resultSet.next()) {
-            return resultSet.getInt("yoru_slap_count");
-        }
-
-        return null;
     }
 }
--- src/main/java/org/bluebadger/libraries/Database.java
+++ src/main/java/org/bluebadger/libraries/Database.java
@@ -1,4 +1,57 @@
 package org.bluebadger.libraries;
 
+import java.sql.*;
+
 public class Database {
+
+    public static void connect(String databaseUrl) throws SQLException {
+        database = new Database(databaseUrl);
+    }
+
+    public static Database getDatabase() {
+        return database;
+    }
+
+    private static final String GET_SINGLE_VALUE_BY_ID_QUERY = "SELECT id, %s FROM discord WHERE id = '%s'";
+
+    private static Database database;
+
+    public Integer getInt(String id, String field) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(String.format(GET_SINGLE_VALUE_BY_ID_QUERY, field, id));
+
+        Integer result = null;
+
+        if (resultSet.next()) {
+            result = resultSet.getInt(field);
+        }
+
+        // Close statement to release resource
+        statement.close();
+
+        return result;
+    }
+
+    public void setInt(String id, String field, int value) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        // Attempt to update value
+        String query = String.format("UPDATE discord SET %s = '%d' WHERE id = '%s'", field, value, id);
+        int totalUpdated = statement.executeUpdate(query);
+
+        // Failed to update, implies that id does not exist so we insert new row
+        if (totalUpdated == 0) {
+            query = "INSERT INTO discord(id, %s) VALUES(%s, %d)";
+            statement.executeUpdate(String.format(query, field, id, value));
+        }
+
+        // Close statement to release resource
+        statement.close();
+    }
+
+    private final Connection connection;
+
+    private Database(String databaseUrl) throws SQLException {
+        connection = DriverManager.getConnection(databaseUrl);
+    }
 }
