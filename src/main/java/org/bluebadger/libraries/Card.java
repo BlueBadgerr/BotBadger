472f67ea806db8dc70cfaa3366833110989e3451
BlueBadgerr <gabriel_lee_92@hotmail.com>
2021-10-04 22:19:22 +1030

    adding card

---
+++ src/main/java/org/bluebadger/libraries/Card.java
@@ -0,0 +1,27 @@
+package org.bluebadger.libraries;
+
+public class Card {
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public enum Rank {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    public enum Suit {
+        SPADES, HEARTS, CLUBS, DIAMONDS
+    }
+}
