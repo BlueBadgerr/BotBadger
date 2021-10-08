f1cb3405bac3c7ceb6c149b4707f724dc9ab5bc9
BlueBadgerr <gabriel_lee_92@hotmail.com>
2021-10-09 00:01:45 +1030

    save interaction hook for updates

--- src/main/java/org/bluebadger/pontoon/PontoonTable.java
+++ src/main/java/org/bluebadger/pontoon/PontoonTable.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.interactions.components.Component;
 import net.dv8tion.jda.api.interactions.components.selections.SelectionMenu;
@@ -24,6 +24,8 @@ public class PontoonTable {
     private Shuffler shuffler = new Shuffler();
     private Player[] players = new Player[MAX_PLAYERS];
     private String description = "This is where a pretty picture of the table should go";
+
+    private InteractionHook latestHook;
 
     // Test only
     public void add(String id) {
@@ -66,6 +68,8 @@ public class PontoonTable {
 
     public void onSlashCommand(SlashCommandEvent event) {
         if (event.getName().equals("pontoon")) {
+            latestHook = event.getHook();
+
             event.replyEmbeds(buildEmbed())
                     .addActionRow(playerOptionsRow)
                     .addActionRow(joinRow)
