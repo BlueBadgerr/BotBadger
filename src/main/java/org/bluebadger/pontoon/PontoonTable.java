e0f8e64fe0b1c060124c7fd835c38ee2c2f54ea9
BlueBadgerr <gabriel_lee_92@hotmail.com>
2021-10-14 21:21:56 +1030

    removed test code

--- src/main/java/org/bluebadger/pontoon/PontoonTable.java
+++ src/main/java/org/bluebadger/pontoon/PontoonTable.java
@@ -27,33 +27,6 @@ public class PontoonTable implements Action {
     private Shuffler shuffler = new Shuffler();
     private Player[] players = new Player[MAX_PLAYERS];
 
-    // Test only
-    public void add(String id) {
-        try {
-            Integer result = database.getInt(id, "yoru_slap_count");
-            int newValue = result + 1;
-            database.setInt(id, "yoru_slap_count", newValue);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Test only
-    public Integer get(String id) {
-        try {
-            return database.getInt(id, "yoru_slap_count");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    // Test only
-    public String draw() {
-        return shuffler.draw().toString();
-    }
-
     public PontoonTable() {
         database = Database.getInstance();
     }
